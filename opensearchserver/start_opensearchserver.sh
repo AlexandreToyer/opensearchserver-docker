cbdd9392cb852f5623382c03579e729a5af3fd1f
Alexandre Toyer <alexandre.toyer@open-searh-server.com>
2014-10-30 09:19:15 +0100

    Update start script

--- opensearchserver/start_opensearchserver.sh
+++ opensearchserver/start_opensearchserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 MEMORY=${MEMORY:-1512m}
+# always stops service
+service opensearchserver stop
 sed -e 's/#JAVA_OPTS="-Xms1G -Xmx1G"/JAVA_OPTS="-Xms'$MEMORY' -Xmx'$MEMORY'"/' -i /etc/opensearchserver
 service opensearchserver start
